0fceef78d7c2bd0fbe7f5165cda7add036d13ac5
Guiio08 <emguiop@libertadores.edu.co>
2022-06-03 10:32:24 -0500

    Aplicacion Heroes En Angular

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { HeroesService } from 'src/app/services/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  heroe:any = {};
+
+  constructor( private activatedRouter:ActivatedRoute,
+                private _heroesServices:HeroesService  
+                ) {
+
+    this.activatedRouter.params.subscribe( params => {
+      this.heroe = this._heroesServices.getHeroe( params['id']);
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
